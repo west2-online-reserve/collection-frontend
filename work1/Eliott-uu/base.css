93d3ffd301faca71c7c9fb2d4d1819ee58862c95
Eliott <138330060+Eliott-uu@users.noreply.github.com>
2023-10-08 16:40:05 +0800

    102201306 - work1 (#21)
    * add: complete work1
    
    * add: complete work1

---
+++ work1/Eliott-uu/base.css
@@ -0,0 +1,213 @@
+body{
+    background-color:#dcf3ff}
+.heade{
+    display: flex;
+    justify-content: space-between;
+    
+ }   
+.header{
+    width:1200px;
+    margin: 0 auto;
+}
+
+.heade li{
+    list-style: none; 
+    display :inline;
+} 
+.heade a{
+    text-decoration: none;
+    color: black;
+    font-size: 30px;
+}
+.heade a:hover{
+    background-color: #104181;
+    border-radius: 50px;
+    text-decoration: underline;
+}
+.introduction {
+    height: 250px;
+    width:1200px;
+    border: 1px solid #1d5bac;
+    border-radius: 50px;
+    position: relative;
+    top: 50px;
+    bottom: 20px;
+    margin: 0 auto
+} 
+.all >img{
+    position: absolute;
+    top: 25px;
+    bottom: 25px;
+    left: 15px;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    overflow:hidden;
+    float:left
+}
+.all1{
+    float:right;
+    position: absolute;
+    top: 100px;
+    bottom: 100px;
+    left: 300px;
+    right: 100px;
+}
+.character{
+    width:1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+    top:75px;
+}
+.character >img:hover{
+    transform: scale(1.5, 1.5);
+}
+.haimianbaobao{
+    float: left;
+    display:inline-block;
+}
+.haimianbaobao1{
+    height:140px;
+    width:300px;
+    background-color:#f8cf37;
+    border-radius: 10%;
+}
+
+.paidaxing{
+    float: left;
+    display:inline-block;
+}
+.paidaxing1{
+    height:140px;
+    width:300px;
+    background-color:#d08079;
+    border-radius: 10%;
+}
+.zhangyuge{
+    float: left;
+    display:inline-block;
+}
+.zhangyuge1{
+    height:140px;
+    width:300px;
+    background-color:#376098;
+    border-radius: 10%;
+}
+.haimianbaobao2 >img{
+    height:300px;
+    width:300px;
+    border-radius: 20%;
+    overflow:hidden;
+    position: relative;
+    top:30px;
+    transition: transform 0.2s;
+}
+
+.paidaxing2 >img{
+    height:300px;
+    width:300px;
+    border-radius: 20%;
+    overflow:hidden;
+    position: relative;
+    top:30px;
+    transition: transform 0.2s;
+}
+
+.zhangyuge2 >img{
+    height:300px;
+    width:300px;
+    border-radius: 20%;
+    overflow:hidden;
+    position: relative;
+    top:30px;
+    transition: transform 0.2s;
+}
+.haimianbaobao2>img:hover{
+    transform: scale(1.3,1.3);
+}
+.paidaxing2>img:hover{
+    transform: scale(1.3,1.3);
+}
+.zhangyuge2>img:hover{
+    transform: scale(1.3,1.3);
+}
+.character p{
+    color: white;
+    text-align: left;
+    margin-left:5px
+}
+.character h1{
+    text-align: center;
+    color: white;
+}
+.td1{
+    background-color:#99ccff;
+}
+.td2{
+    background-color: #87ceeb;
+}
+.td3{
+    background-color:#6699cc;
+}
+.series{
+    width:1200px;
+    margin: 0 auto;
+    position: relative;
+    top:150px;
+}
+.l1{
+    width:720px;
+}
+.l2{
+    width:360px;
+}
+.l3{
+    width:120px;
+}
+.series td{
+    text-align: center;
+}
+.suggestion{
+    width: 1200px;
+    margin: 0 auto;
+    position: relative;
+    top:250px;
+}
+.suggestion textarea {
+    width: 1200px;
+    height: 240px;
+    background-color:#dcf3ff ;
+    border: 1px solid #a8dcf4;
+    border-radius: 30px;
+    font-size: 18px;
+    padding: 20px;
+    font-weight: 700;
+}
+.last{
+    width: 200px;
+    height: 80px;
+    background-color:#6495ed;
+    border-radius: 10%;
+    color: white;
+    font-size: 20px;
+    font-weight: 700;
+    position: relative;
+    top:25px;
+    float:right;
+}
+.last:hover{
+    background-color: #376098;
+}
+.container{
+    position: relative;
+    top:25px;
+    text-align: center;
+    color: white;
+    text-decoration: none;
+    display: block;
+    height: 100%;
+    width: 100%;
+}
+

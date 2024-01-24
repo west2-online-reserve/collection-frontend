8656c5d8f8476cb72a62b8771f82616c8c5d5ba0
IMZTTTT <126803308+Imztttt-XD@users.noreply.github.com>
2024-01-24 23:51:49 +0800

    832203314 - work3 (#66)
    * add: complete work1
    
    * add:complete work2
    
    * add:complete work2
    
    * 832203314 - work2
    
    * 832203314 - work2
    
    * 832203314 - work2
    
    * 832203314 - work3
    
    * 832203314 - work3(重置版)

---
+++ work3/IMZTTTT_XD/TODOLIST/src/views/Login.vue
@@ -0,0 +1,264 @@
+<script  lang='ts' setup>
+import { useRouter } from 'vue-router';
+import { ref, reactive } from 'vue'
+import { FormInstance } from 'element-plus';
+import { useUserStore } from '../stores/UserStore';
+import { ElMessage } from 'element-plus'
+const router = useRouter()
+const userStore = useUserStore();
+
+//控制注册与登录表单的显示， 默认显示注册
+const ruleFormRef = ref<FormInstance>()
+
+// 定义表单校验规则
+const rules = reactive({
+    username: [
+        { required: true, message: "必填" },
+        { min: 6, message: '用户格式无效', trigger: 'blur' }
+    ],
+    email: [
+        { required: true, message: "必填" },
+        {
+            pattern:
+                /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
+            message: '邮箱格式无效', trigger: 'blur'
+        }
+
+    ],
+    password: [
+        { required: true, message: "必填" },
+        {
+            pattern:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@#!%^*?&+-]{8,}/,
+            message: '密码格式无效', trigger: 'blur'
+        }
+    ]
+})
+
+//定义注册信息数据类型
+const registerData = reactive({
+    username: '',
+    email: '',
+    password: ''
+})
+
+// 用来跳转登录和注册
+const isRegister = ref(false);
+
+// 一些消息提示
+const Registsuccess = () => {
+    ElMessage({
+        message: '注册成功',
+        type: 'success',
+    });
+}
+const Loginsuccess = () => {
+    ElMessage({
+        message: '登陆成功',
+        type: 'success',
+    })
+}
+const Loginerror = () => {
+    ElMessage.error('用户用户名或密码错误')
+}
+
+// 用户注册，校验是否符合规则，然后提交信息到pinia
+const register = (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    formEl.validate((valid) => {
+        if (valid) {
+            userStore.addRegisterData(registerData);
+            Registsuccess();
+            isRegister.value = false;
+        } else {
+            return false;
+        }
+    });
+};
+// 用户登录，并校对登录信息是否正确，然后储存到pinia
+const login = (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    formEl.validate((valid) => {
+        if (valid) {
+            const user = userStore.logincheck(
+                registerData.username,
+                registerData.password
+            );
+            if (user) {
+                Loginsuccess();
+                userStore.setLoginData(registerData);
+                router.push('/menu/todolist')
+            } else {
+                Loginerror();
+                return false
+            }
+        } else {
+            return false;
+        }
+    });
+};
+// 清空注册数据
+const clearRegisterData = () => {
+    registerData.username = ''
+    registerData.email = ''
+    registerData.password = ''
+}
+
+</script>
+
+<template>
+    <div class="bg">
+        <el-container>
+            <el-main>
+                <!-- 注册表单 -->
+                <el-form ref="ruleFormRef" autocomplete="off" v-if="isRegister" label-position=top :model="registerData"
+                    :rules="rules">
+                    <span class="title">Nice to meet you</span>
+                    <br>
+                    <span class="detail">Please enter your details</span>
+                    <br>
+                    <el-form-item label="用户名" prop="username">
+                        <el-input placeholder="用户名长度不小于6位" v-model="registerData.username"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input placeholder="邮箱格式需正确" v-model="registerData.email"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input placeholder="密码长度不小于8位，同时包含大小写" v-model="registerData.password"
+                            type="password"></el-input>
+                    </el-form-item>
+                    <!-- 注册按钮 -->
+                    <el-form-item>
+                        <el-button class="button" type="primary" auto-insert-space @click="register(ruleFormRef)">
+                            注册
+                        </el-button>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-link type="info" :underline="false" @click="isRegister = false; clearRegisterData()">
+                            已经注册账号？点此登录
+                        </el-link>
+                    </el-form-item>
+                </el-form>
+                <!-- 登录表单 -->
+                <el-form ref="ruleFormRef" autocomplete="off" v-else label-position=top label-width="100px"
+                    style="max-width: 460px" :model="registerData" :rules="rules">
+                    <span class="title">Welcome Back</span>
+                    <br>
+                    <span class="detail">Please enter your details</span>
+                    <br>
+                    <el-form-item label="用户名" prop="username">
+                        <el-input placeholder="请输入用户名" v-model="registerData.username"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input name="password" type="password" placeholder="请输入密码"
+                            v-model="registerData.password"></el-input>
+                    </el-form-item>
+                    <el-form-item class="flex">
+                        <div class="flex">
+                            <el-checkbox>30天内免登录</el-checkbox>
+                            <el-link type="primary" :underline="false">忘记密码</el-link>
+                        </div>
+                    </el-form-item>
+                    <!-- 登录按钮 -->
+                    <el-form-item>
+                        <el-button class="button" type="primary" auto-insert-space
+                            @click="login(ruleFormRef)">登录</el-button>
+                    </el-form-item>
+                    <el-form-item style="justify-content: center;">
+                        <el-link type="info" :underline="false" @click="isRegister = true; clearRegisterData()">
+                            还没有注册账号？点击注册
+                        </el-link>
+                    </el-form-item>
+                </el-form>
+            </el-main>
+            <img src="../assets/621114a7474f50daf3d8f5c0002f23d8 1.png" alt="">
+        </el-container>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+/* 样式 */
+.bg {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #DDEBDB;
+}
+
+.el-container {
+    flex: none;
+    display: flex;
+    align-items: center;
+    border-radius: 10px;
+    background: #D1E2CF;
+    width: 1300px;
+}
+
+.el-main {
+    flex: none;
+    display: flex;
+    justify-content: center;
+    background: #FAFCF9;
+    width: 670px;
+    height: 83vh;
+    box-sizing: border-box;
+}
+
+img {
+    position: relative;
+    left: 52px;
+    top: -21px;
+}
+
+.el-form {
+    margin-top: 140px;
+    width: 320px;
+    text-align: center;
+}
+
+.button {
+    width: 320px;
+    height: 34px;
+    border-radius: 5px;
+    background: #456442;
+}
+
+.title {
+    color: #426440;
+    font-family: Inter;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+.detail {
+    color: #3D565A;
+    font-family: Inter;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+.el-form-item {
+    margin-top: 20px;
+}
+
+.flex {
+    width: 320px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.el-input {
+    border-radius: 5px;
+    border: 2px solid #426440;
+    background: #FEFFFF;
+    box-shadow: none;
+    --el-input-bg-color: none;
+    --el-input-hover-border-color: none;
+    --el-input-focus-border-color: none;
+}
+</style>
---
+++ work3/IMZTTTT_XD/TODOLIST/src/views/menu/form.vue
@@ -0,0 +1,266 @@
+<script lang="ts" setup>
+import { computed, ref } from 'vue'
+import { Search } from '@element-plus/icons-vue'
+import { useTodoStore } from '../../stores/TodoStore';
+import { FormInstance } from 'element-plus'
+import { ElMessage } from 'element-plus'
+
+const ruleFormRef = ref<FormInstance>()
+const search = ref('')
+const date = ref('')
+const todoStore = useTodoStore()
+
+// 一些消息提示
+const Editerror = () => {
+    ElMessage.error('输入不能为空，请输入！')
+}
+const Deletesuccess = () => {
+    ElMessage({
+        message: '删除成功',
+        type: 'success',
+    })
+}
+const Editsuccess = () => {
+    ElMessage({
+        message: '修改成功',
+        type: 'success',
+    })
+}
+
+// 从pinia获取待办数据
+const tableData = computed(() => {
+    const incompleted = todoStore.todolistStore.map((item,index) => ({
+        id:index++,
+        content: item.content,
+        start: item.time[0],
+        end: item.time.length > 1 ? item.time[1] : '',
+        completed: false,
+    }))
+    const completed = todoStore.completeStore.map((item,index) => ({
+        id: index++,
+        content: item.content,
+        start: item.time[0],
+        end: item.time.length > 1 ? item.time[1] : '',
+        completed: true,
+    }));
+    return [...incompleted, ...completed];
+
+});
+
+// 表格数据
+const filterTableData = computed(() => {
+    const start = (currentPage.value - 1) * pageSize.value;
+    const end = start + pageSize.value;
+    return tableData.value
+        .filter(
+            (data) =>
+                (!date.value || data.start.includes(date.value) || data.end.includes(date.value)) &&
+                (!search.value || data.content.toLowerCase().includes(search.value.toLowerCase()))
+        )
+    .slice(start, end);
+});
+
+// 修改操作
+const editData = ref({
+    id: 0,
+    content: '',
+})
+const handleEdit = ({ row }) => {
+    dialogVisible.value = true;
+    editData.value.content = row.content;
+    editData.value.id = row.id;
+}
+//控制添加分类弹窗
+const dialogVisible = ref(false)
+// 校验修改信息
+const rules = {
+    content: [
+        { required: true, message: '请输入内容', trigger: 'blur' },
+    ],
+}
+// 修改pinia内存储的待办信息
+const editTodo = (formEl: FormInstance | undefined) => {
+    if (!formEl) return;
+    formEl.validate((valid) => {
+        if (valid) {          
+            todoStore.EditTodoById(editData.value.id, editData.value.content);
+            dialogVisible.value = false;
+            Editsuccess()
+        } else {
+            Editerror();
+            return false;
+        }
+    });
+};
+
+// 完成操作
+const handleComplish = ({ row }) => {
+    const completedTask = todoStore.todolistStore[row.id];
+    todoStore.completelist(completedTask);
+    todoStore.todolistStore.splice(row.id, 1);
+}
+
+// 删除操作
+const handleDelete = ({ row }) => {
+    todoStore.todolistStore.splice(row.id, 1);
+    Deletesuccess();
+}
+
+// 分页
+const currentPage = ref(1); // 初始页
+const pageSize = ref(8); // 初始每页的数据
+const handleSizeChange = (size) => {
+    pageSize.value = size;
+};
+
+const handleCurrentChange = (page) => {
+    currentPage.value = page;
+};
+
+</script>
+<template>
+    <div class="main" style="margin-left:120px; margin-top: 50px;">
+        <div class="title">
+            <span>待办事项</span>
+            <div class="demo-date-picker" style="display: inline;margin-left: 30px;">
+                <el-date-picker v-model="date" type="date" placeholder="请选择要查找的日期" format="YYYY/MM/DD"
+                    value-format="YYYY/MM/DD" />
+                <el-input v-model="search" placeholder="请输入要查找的内容" :prefix-icon="Search"
+                    style="--el-input-width: none;padding-left: 20px;display: inline;" />
+            </div>
+        </div>
+        <!-- 表格 -->
+        <el-table :data="filterTableData" empty-text="暂无内容">
+            <!-- 表头 -->
+            <el-table-column type="index" label="序号" width="120px" align="center" />
+            <el-table-column label="内容" prop="content" width="478px" align="center" />
+            <el-table-column label="创建时间" prop="start" width="250px" align="center" format="YYYY/MM/DD" />
+            <el-table-column label="完成时间" prop="end" width="250px" align="center" />
+            <el-table-column label="操作" align="center" width="298px">
+                <!-- 操作按钮 -->
+                <template #default="scope">
+                    <el-button size="large" type="primary" @click="handleComplish(scope)" v-if="!scope.row.completed"
+                        style="background-color: #426440;font-size: 20px;font-style: normal;font-weight: 700;"
+                        round>完成</el-button>
+                    <el-button size="large" type="primary" @click="handleEdit(scope)"
+                        v-if="!scope.row.completed" style="font-size: 20px;font-style: normal;font-weight: 700;"
+                        round>修改</el-button>
+                    <el-button size="large" type="danger" @click="handleDelete(scope)" v-if="!scope.row.completed"
+                        style="font-size: 20px;font-style: normal;font-weight: 700;" round>删除</el-button>
+                    <el-button size="large" type="warning" @click="handleDelete(scope)" v-if="scope.row.completed"
+                        style="font-size: 20px;font-style: normal;font-weight: 700;width: 130px;" round
+                        disabled>已完成</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <!-- 分页 -->
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage"
+            layout="prev, pager, next" :total="tableData.length" :page-size="pageSize" style="float: right;"/>
+        <!-- 修改弹窗 -->
+        <el-dialog v-model="dialogVisible" title="修改" width="30%">
+            <el-form ref="ruleFormRef" :model="editData" :rules="rules" label-width="100px" style="padding-right: 30px">
+                <el-form-item label="待办事项" prop="content">
+                    <el-input v-model="editData.content" minlength="1" maxlength="20"></el-input>
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogVisible = false">取消</el-button>
+                    <el-button type="primary" @click="editTodo(ruleFormRef)"> 确认 </el-button>
+                </span>
+            </template>
+        </el-dialog>
+    </div>
+</template>
+<style lang="scss" scoped>
+.title {
+    color: #11240E;
+    font-size: 32px;
+    font-weight: 800;
+    margin: 20px 0;
+}
+
+:deep(.el-table) {
+    overflow: hidden;
+    width: 1398px;
+    text-align: center;
+    border: 2px solid #D1E2CF;
+    border-left:none ;
+    border-top:none ;
+    box-sizing: border-box;
+}
+
+:deep(.el-table__cell) {
+    padding: 5px;
+    height: 70px;
+    border: 2px solid #D1E2CF;
+    box-sizing: border-box;
+}
+
+:deep(.el-table__header .cell) {
+    color: #11240E;
+    font-family: Inter;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    overflow: visible;
+}
+
+:deep(.el-table__body .cell) {
+    color: #5A6858;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    overflow: visible;
+}
+
+// .el-table_column_1 .cell {
+//     color: #11240E;
+//     font-size: 28px;
+//     font-weight: 500;
+// }
+
+
+
+:deep(.el-table__empty-block ){
+    width: 1390px !important;
+    height: 500px !important;
+    border-top: 2px solid #D1E2CF;
+    border-left: 2px solid #D1E2CF;
+}
+
+:deep(.el-table__empty-text) {
+    color: #D2D2D2;
+    font-size: 32px;
+    font-weight: 700;
+}
+
+:deep(.completed-task .cell) {
+    color: #D3D3D3;
+}
+
+/* 日期选择器 */
+.demo-date-picker .cell {
+    height: 30px;
+    padding: 3px 0;
+    box-sizing: border-box;
+}
+
+.demo-date-picker .cell .text {
+    width: 24px;
+    height: 24px;
+    display: block;
+    margin: 0 auto;
+    line-height: 24px;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    border-radius: 50%;
+}
+</style>
+  
+
+  

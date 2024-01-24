fcda262f2c6da8c8cc22ffea04b7c82fbbb3da5e
xhhing <143531614+xhhing@users.noreply.github.com>
2024-01-25 01:20:39 +0800

    102201429 - work3 (#63)
    * add: complete work0
    
    * add: complete work1
    
    * add: complete work1
    
    * add: complete work2
    
    * add:one part
    
    * add:complete work3

---
+++ work3/xhhing/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+<router-view />
+
+</template>
+
+<script setup>
+
+
+
+</script>
+
+<style>
+
+#app{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+</style>

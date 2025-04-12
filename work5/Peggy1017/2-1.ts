792a1973a6c8d7c33c489abb50f6d319a088471c
Peggy1017 <3302552671@qq.com>
2025-04-12 22:16:39 +0800

    add:complete work5

---
+++ work5/Peggy1017/2-1.ts
@@ -0,0 +1,4 @@
+// 请补全参数类型和返回类型
+function add(a:number, b:number):number {
+    return a + b;
+  }
---
+++ work5/Peggy1017/2-2.ts
@@ -0,0 +1,3 @@
+type Fn = (a: number, b: number) => number;  // 请补全函数类型
+
+const fn: Fn = (a, b) => a + b;
